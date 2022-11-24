b9d213a785981e1319a8e108e86974ac35262b47
Gabriel Barbosa <gabrielbarbosadorio@gmail.com>
2022-11-24 14:03:20 -0300

    Esqueleto do flashDeals feito

--- src/components/FlashDeals/FlashDeals.tsx
+++ src/components/FlashDeals/FlashDeals.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import FlashCard from "./FlashCard";
 
 const FlashDeals = () => {
   return (
-    <div>FlashDeals</div>
-  )
-}
+    <>
+      <section className="flash background">
+        <div className="container">
+          <div className="heading f_flex">
+            <i className="fa fa-bolt"></i>
+            <h1>Flash Deals</h1>
+          </div>
+          <FlashCard />
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default FlashDeals
+export default FlashDeals;
