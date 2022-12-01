2d241eae1167091c1a05ee29b1e70f7d24cced22
Gabriel Barbosa <gabrielbarbosadorio@gmail.com>
2022-12-01 18:58:07 -0300

    Casca do TopCate feita

--- src/components/Top/TopCate.tsx
+++ src/components/Top/TopCate.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const TopCate = () => {
   return (
-    <div>TopCate</div>
-  )
-}
+    <>
+      <section className="topCat background">
+        <div className="container">
+          <div className="heading d_flex">
 
-export default TopCate
+            <div className="heading-left row f_flex">
+            <i className="fa-solid fa-border-all"></i>
+                <h2>Top Categorias</h2>
+            </div>
+
+            <div className="heading-right row">
+                <span>Ver Todas</span>
+                <i className="fa fa-caret-right"></i>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default TopCate;
--- src/pages/Pages.tsx
+++ src/pages/Pages.tsx
@@ -1,12 +1,13 @@
-import { useContext } from "react";
 import FlashDeals from "../components/FlashDeals/FlashDeals";
 import Home from "../components/MainPage/Home";
+import TopCate from "../components/Top/TopCate";
 
 const Pages = ({ productItems }: any) => {
   return (
     <>
       <Home />
       <FlashDeals productItems={productItems}  />
+      <TopCate/>
     </>
   );
 };
