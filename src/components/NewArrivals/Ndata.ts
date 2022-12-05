bd3a689947103ed10f9089c881182cc41d892ad3
Gabriel Barbosa <gabrielbarbosadorio@gmail.com>
2022-12-05 12:49:55 -0300

    dados do newarrivals adicionados

---
+++ src/components/NewArrivals/Ndata.ts
@@ -0,0 +1,34 @@
+const Ndata = [
+    {
+        cover: "",
+        name: "Óculos de sol",
+        price: "150",
+    },
+    {
+        cover: "",
+        name: "Maquiagem",
+        price: "250",
+    },
+    {
+        cover: "",
+        name: "Smart Watch",
+        price: "50",
+    },
+    {
+        cover: "",
+        name: "Batom",
+        price: "15",
+    },
+    {
+        cover: "",
+        name: "Planta verde",
+        price: "10",
+    },
+    {
+        cover: "",
+        name: "Árvore bonsai",
+        price: "400",
+    },
+]
+
+export default Ndata
