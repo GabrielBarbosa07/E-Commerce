c9d3c94c4ff529a87931bf8f6e3321b1370f50ad
Gabriel Barbosa <gabrielbarbosadorio@gmail.com>
2022-12-06 20:18:49 -0300

    Tamanho da imagens reajustadas

--- src/components/NewArrivals/Ndata.ts
+++ src/components/NewArrivals/Ndata.ts
@@ -1,31 +1,37 @@
+import SmartWatch from "./assets/smart-watch.jpg"
+import Maquiagem from "./assets/maquiagem.jpg"
+import OculosDeSol from "./assets/oculosDeSol.jpg"
+import Batom from "./assets/batom.jpg"
+import Planta from "./assets/planta.jpg"
+import Arvore from "./assets/arvore.jpg"
 const Ndata = [
     {
-        cover: "https://images.pexels.com/photos/1071878/pexels-photo-1071878.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        cover: OculosDeSol,
         name: "Óculos de sol",
         price: "150",
     },
     {
-        cover: "https://images.pexels.com/photos/1071878/pexels-photo-1071878.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        cover: Maquiagem,
         name: "Maquiagem",
         price: "250",
     },
     {
-        cover: "https://images.pexels.com/photos/1071878/pexels-photo-1071878.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        cover: SmartWatch,
         name: "Smart Watch",
         price: "50",
     },
     {
-        cover: "https://images.pexels.com/photos/1071878/pexels-photo-1071878.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        cover: Batom,
         name: "Batom",
         price: "15",
     },
     {
-        cover: "https://images.pexels.com/photos/1071878/pexels-photo-1071878.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        cover: Planta,
         name: "Planta verde",
         price: "10",
     },
     {
-        cover: "https://images.pexels.com/photos/1071878/pexels-photo-1071878.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        cover: Arvore,
         name: "Árvore bonsai",
         price: "400",
     },
--- src/components/NewArrivals/NewArrivals.tsx
+++ src/components/NewArrivals/NewArrivals.tsx
@@ -7,11 +7,11 @@ const NewArrivals = () => {
       <section className="NewArrivals background">
         <div className="container">
           <div className="heading d_flex">
-            <div className="heading-left row  f_flex">
+            <div className="heading-left row f_flex">
               <img src="https://img.icons8.com/glyph-neue/64/26e07f/new.png" />
               <h2>Novos Produtos </h2>
             </div>
-            <div className="heading-right row ">
+            <div className="heading-right row">
               <span>Ver Todos</span>
               <i className="fa-solid fa-caret-right"></i>
             </div>
