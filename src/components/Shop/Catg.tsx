f5fbd4d346b911dac2ee11bf682b1a323578cfc9
Gabriel Barbosa <gabrielbarbosadorio@gmail.com>
2022-12-10 18:29:00 -0300

    Categorias do shop adicionado, falta adicionar os icones nas categorias

--- src/components/Shop/Catg.tsx
+++ src/components/Shop/Catg.tsx
@@ -1,7 +1,51 @@
 const Catg = () => {
+  const data = [
+    {
+      cateImg: "",
+      cateName: "Apple",
+    },
+    {
+      cateImg: "",
+      cateName: "Samsung",
+    },
+    {
+      cateImg: "",
+      cateName: "Oppo",
+    },
+    {
+      cateImg: "",
+      cateName: "Vivo",
+    },
+    {
+      cateImg: "",
+      cateName: "Redmi",
+    },
+    {
+      cateImg: "",
+      cateName: "Sony",
+    },
+  ];
   return (
-    <div>Catg</div>
-  )
-}
+    <>
+      <div className="category">
+        <div className="chead d_flex">
+          <h1>Brands </h1>
+          <h1>Shops </h1>
+        </div>
+        {data.map(({ cateImg, cateName }, index) => {
+          return (
+            <div className="box f_flex" key={index}>
+              <img src={cateImg} alt="" />
+              <span>{cateName}</span>
+            </div>
+          );
+        })}
+        <div className="box box2">
+          <button>Ver todas as marcas</button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Catg
+export default Catg;
